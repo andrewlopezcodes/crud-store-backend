91fc548fc9f6d52de10e143a9611c497f05fcadb
mrandrewlopez <andrewlopezcodes@gmail.com>
2018-06-15 11:33:40 -0400

    added cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.json({
